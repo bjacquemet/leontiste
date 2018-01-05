f3ce9d8a440c537fe69cb4743899aece3fe31c45
Baptiste Jacquemet <baptiste.jacquemet@gmail.com>
2018-01-05 16:52:10 +1300

    add lightbox for img-fluid

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,22 +18,22 @@
 
 
 $(document).on('ready', function () {
+
+  // LOGO ANIMATION START
   $width = $('#middle').width();
   $leon = parseInt($width / 2);
   $tiste = $width - $leon;
-  // $('#leon').width($('#leon').width());
   $('#leon')
     .css('-webkit-transform', 'translate('+ $leon +'px)')
     .css('transform', 'translate('+ $leon +'px)')
     .css('-moz-transform', 'translate('+ $leon +'px)')
     .css('-o-transform', 'translate('+ $leon +'px)')
     .css('-ms-transform', 'translate('+ $leon +'px)');
-  console.log('load');
   $('#tiste')
     .css('-webkit-transform', 'translate(-'+ $tiste +'px)')
     .css('transform', 'translate(-'+ $tiste +'px)')
     .css('-moz-transform', 'translate(-'+ $tiste +'px)')
     .css('-o-transform', 'translate(-'+ $tiste +'px)')
     .css('-ms-transform', 'translate(-'+ $tiste +'px)');
-  // setTimeout(function(){$("body").removeClass("preload");}, 1000);
+  // LOGO ANIMATION END
 })
--- app/assets/javascripts/cartes.coffee
+++ app/assets/javascripts/cartes.coffee
@@ -1,3 +1,13 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).delegate '.main-content .img-fluid', 'click', (event) ->
+  event.preventDefault()
+  $src = $(this).attr("src")
+  $alt = $(this).attr("alt")
+  $title = $(this).attr("title")
+  $('#img-modal .modal-body').empty()
+  $('#img-modal .modal-body').append('<img src="' + $src + '" alt="' + $alt + '" title="'+ $title + '" class="img-fluid">')
+  $("#img-modal").modal()
+  return
